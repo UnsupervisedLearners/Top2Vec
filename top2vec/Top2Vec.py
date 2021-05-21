6c39280488c7f5a9af39688087049b63e77a442d
jsmccid <jxmccarthy@gmail.com>
2021-05-22 03:38:54 +1000

    added umap_model and cluster to self

--- top2vec/Top2Vec.py
+++ top2vec/Top2Vec.py
@@ -357,7 +357,9 @@ class Top2Vec:
                          'metric': 'cosine'}
 
         umap_model = umap.UMAP(**umap_args).fit(self._get_document_vectors(norm=False))
-
+        ## sketchy addition
+        self.__umap_model = umap_model
+        
         # find dense areas of document vectors
         logger.info('Finding dense areas of documents')
 
@@ -367,6 +369,8 @@ class Top2Vec:
                              'cluster_selection_method': 'eom'}
 
         cluster = hdbscan.HDBSCAN(**hdbscan_args).fit(umap_model.embedding_)
+        ## sketchy addition 2
+        self.__cluster = cluster
 
         # calculate topic vectors from dense areas of documents
         logger.info('Finding topics')
